99d4d2c61de6ae8c41d83844d2292d02fd52ec1b
chakradhar-madishetty <chakradharmadishetti111@gmail.com>
2024-07-31 00:39:32 +0530

    portfolio

---
+++ assets/js/main.js
@@ -0,0 +1,39 @@
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
+
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show");
+    });
+  }
+};
+showMenu("nav-toggle", "nav-menu");
+
+const navLinks = document.querySelectorAll(".nav-link");
+const sections = document.querySelectorAll(".section");
+
+window.addEventListener("scroll", () => {
+  let current = '';
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (scrollY >= sectionTop - 390) {
+      current = section.getAttribute('id');
+    }
+  })
+
+  navLinks.forEach(link => {
+    link.classList.remove('active');
+    if (link.classList.contains(current)) {
+      link.classList.add('active');
+    }
+  })
+})
+
+
+const navMenu = document.getElementById("nav-menu");
+navLinks.forEach((n) => n.addEventListener("click", () => { navMenu.classList.remove("show") }));
+
+
+
